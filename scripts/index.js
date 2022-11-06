8723227ce2165cfdeedaa51d0065e087c1410f19
Danila Semenov <inflames147@gmail.com>
2022-11-06 22:34:59 +0300

    исправить замечани после первой итерации

--- scripts/index.js
+++ scripts/index.js
@@ -1,41 +1,38 @@
 let popupElem = document.querySelector('.popup');
 let editElem = document.querySelector('.profile__edit');
 let closeElem = document.querySelector('.popup__close');
+let nameProfile = document.querySelector('.profile__name');
+let aboutProfile = document.querySelector('.profile__about');
+let nameInfo = document.querySelector('.popup__info_data_name');
+let aboutInfo = document.querySelector('.popup__info_data_about');
+let saveElem = document.querySelector('.popup__save');
+let formElem = document.querySelector('.popup__container')
 
 function openPopup() {
   popupElem.classList.add('popup__opened');
+
+  nameInfo.value.textContent = nameProfile;
+
+  aboutInfo.value.textContent = aboutProfile;
 }
 
 editElem.addEventListener('click', openPopup);
 
-function closePopup(evt) {
-  evt.preventDefault();
-
+function closePopup() {
   popupElem.classList.remove('popup__opened');
 }
 
 closeElem.addEventListener('click', closePopup);
 
-let nameProfile = document.querySelector('.profile__name');
-let aboutProfile = document.querySelector('.profile__about');
-
 function saveInfo(evt) {
   evt.preventDefault();
-
-  let nameInfo = document.querySelector('.popup__info_data_name');
-
-  let aboutInfo = document.querySelector('.popup__info_data_about');
 
   nameProfile.textContent = nameInfo.value;
 
   aboutProfile.textContent = aboutInfo.value;
 
-  popupElem.classList.remove('popup__opened');
+  closePopup();
 }
 
-let saveElem = document.querySelector('.popup__save');
-
-saveElem.addEventListener('click', saveInfo);
-
-saveElem.addEventListener('submit', saveInfo);
+formElem.addEventListener('submit', saveInfo);
 
